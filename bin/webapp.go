16f0f7c7f5a7a5eb073983d021a305504226ab36
Yrineu Rodrigues <yfrfelipe@gmail.com>
2018-07-14 23:11:16 -0700

    First commit
    Signed-off-by: Yrineu Rodrigues <yfrfelipe@gmail.com>

---
+++ bin/webapp.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"fmt"
+	"html/template"
+	"io/ioutil"
+	"log"
+	"net/http"
+)
+
+type Page struct {
+	Title string
+	Body  []byte
+}
+
+func handler(writer http.ResponseWriter, request *http.Request) {
+	fmt.Fprintln(writer, "Hi, %s", request.URL.Path[1:])
+}
+
+func loadPage(title string) *Page {
+	filename := title + ".html"
+	body, _ := ioutil.ReadFile(filename)
+	return &Page{Title: title, Body: body}
+}
+
+func editHandler(writer http.ResponseWriter, request *http.Request) {
+	template, _ := template.ParseFiles("http/index.html")
+	template.Execute(writer, &Page{Title: "Opa"})
+}
+
+func main() {
+	http.HandleFunc("/", editHandler)
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
